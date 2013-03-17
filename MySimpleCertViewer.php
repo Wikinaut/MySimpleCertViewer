f7ebd0fe0028fe2a88cd4df76bc6ade6d73feb40
Tyler Anthony Romeo <tylerromeo@gmail.com>
2013-03-17 15:58:27 -0400

    Changed exec to use PHP streams. Put into function.
    Replaced manual exec of openssl with sed to use
    PHP's SSL streams and client certificate capture
    functionality. Also, put code into a function
    so that it's not just a standalone script.

--- MySimpleCertViewer.php
+++ MySimpleCertViewer.php
@@ -9,65 +9,53 @@
  * http://www.opensource.org/licenses/mit-license.php
  *
  */
-$version = "1.01 20130317";
+define( 'CERTVIEWER_VERSION', "1.01 20130317" );
 
-# The server and port are currently hardcoded
-#
-$server = "www.google.org";
-$port = "443";
+function getCertificateInfo( $server, $port = 443, $timeout = false ) {
+	$context = stream_context_create( array( 'ssl' => array(
+		'capture_peer_cert' => true
+	) ) );
 
-$certFilename = tempnam( "", "server-certificate-");
-exec( "openssl s_client -connect $server:$port 2>/dev/null </dev/null " .
-	"| sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' 1> $certFilename",
-	$out
-);
-date_default_timezone_set('UTC');
-$now = time();
+	$timeout = $timeout ?: ini_get( "default_socket_timeout" );
+	$errno = $errstr = 0;
+	$fp = stream_socket_client(
+		"ssl://$server:$port",
+		$errno,
+		$errstr,
+		$timeout,
+		STREAM_CLIENT_CONNECT,
+		$context
+	);
 
-function decodeCert( $cert ) {
-    $cert = preg_replace( '/\-+BEGIN CERTIFICATE\-+/', '', $cert );
-    $cert = preg_replace( '/\-+END CERTIFICATE\-+/', '', $cert );
-    $cert = str_replace( array( "\n\r", "\n", "\r" ), '', trim( $cert ) );
-    return base64_decode( $cert );
- }
+	$params = stream_context_get_params( $fp );
+	fclose( $fp );
+	$cp = $params['options']['ssl']['peer_certificate'];
+	$cert = '';
+	openssl_x509_export( $cp, $cert );
+	$certArray = openssl_x509_parse( $cp );
+	openssl_x509_free( $cp );
 
-$cert = file_get_contents( $certFilename );
-$decCert = decodeCert( $cert );
+	$now = time();
+	$certArray['x-server-port'] = "$server:$port";
+	$certArray['x-server'] = $server;
+	$certArray['x-port'] = $port;
+	$certArray['x-retrieval-time'] = array(
+		'utc' => date( "YmdHis", $now ) . "Z",
+		'unix' => date( "U", $now),
+	);
+	$certArray['extensions']['x-subjectAltName'] = explode( ",", $certArray['extensions']['subjectAltName'] );
+	$certArray['x-certificate-base64'] = $cert;
+	$certArray['x-mysimplecertviewer-version'] = CERTVIEWER_VERSION;
 
-$certArray = array();
-$certArray['x-server-port'] = "$server:$port";
-$certArray['x-server'] = $server;
-$certArray['x-port'] = $port;
-$certArray['x-retrieval-time'] = array(
-	'utc' => date( "YmdHis", $now ) . "Z",
-	'unix' => date( "U", $now),
-);
-$certArray['x-mysimplecertviewer-version'] = $version;
+	// Decode the certificate to get fingerprints.
+	$decCert = preg_replace( '/\-+(BEGIN|END) CERTIFICATE\-+/', '', $cert );
+	$decCert = str_replace( array( "\n\r", "\n", "\r" ), '', trim( $cert ) );
+	$decCert = base64_decode( $cert );
+	$certArray['x-fingerprints'] = array(
+		"sha1" => sha1( $decCert ),
+		"md5" => md5( $decCert ),
+		"sha256" => hash( 'sha256', $decCert ),
+	);
 
-$certArray['x-fingerprints'] = array(
-	"sha1" => sha1( $decCert ),
-	"md5" => md5( $decCert ),
-	"sha256" => sha256( $decCert ),
-);
-
-$certArray = $certArray + openssl_x509_parse( $cert );
-$subjectAltName = $certArray['extensions']['subjectAltName'];
-$certArray['extensions']['x-subjectAltName'] = explode( ",", $subjectAltName );
-$certArray['x-certificate-base64'] = $cert;
-
-$retrievalTimestamp = date( "Y:m:d H:i:s e", $now );
-$output = print_r( $certArray , true );
-
-header( "Content-Type: text/html" );
-echo <<<EOF
-<h2>MySimpleCertViewer</h2>
-<pre>
-<a href="https://github.com/Wikinaut/MySimpleCertViewer">source code on GitHub</a>
-<hr>
-Certificate data for <b><a href="https://$server:$port">https://$server:$port</a></b> as of $retrievalTimestamp
-(x-fields are added by MySimpleCertViewer)
-
-$output
-</pre>
-EOF;
-
+	return $certArray;
+}
